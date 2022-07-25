51fa5202db9095f568020fb4677bd867bba6e81d
Manthan-Thakkar <62873934+Manthan-Thakkar@users.noreply.github.com>
2022-07-26 00:59:18 +0530

    Delete s3_download_file.py

--- s3_download_file.py
+++
@@ -1,7 +0,0 @@
-from urllib import response
-import boto3
-
-s3_client = boto3.client('s3')
-
-response = s3_client.download_file(
-    'mys3boto3bucket', 'Manthan.pdf', r'C:\Users\Manthan\Documents\Manthan.pdf')
